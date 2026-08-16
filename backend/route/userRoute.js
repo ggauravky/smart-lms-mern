0dffc60529bbe2ea855c996db2fc4860f966e141
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-08-16 18:48:46 +0530

    solve a import bug

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -2,6 +2,8 @@ import express from 'express';
 import isAuth from "../middleware/isAuth.js";
 import { getCurrentUser, updateProfile } from "../controller/userController.js";
 import multer from 'multer';
+import upload from '../middleware/multer.js';
+
 const userRouter = express.Router();
 
 userRouter.get('/getCurrentUser', isAuth, getCurrentUser);
