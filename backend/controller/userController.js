6ca912cf45f2e94b8ad02a2125639ff87b5259a8
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-08-16 18:43:51 +0530

    updated Image upload using multer & Cloudinary Creating Update Profile Controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,15 +1,40 @@
 import User from "../models/userModel.js";
+import uploadOnCloudinary from "../config/cloudinary.js";
 
 export const getCurrentUser = async (req, res) => {
-    try {
-        const user  = await User.findById(req.userId).select('-password');
-        if(!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        return res.status(200).json(user);
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error("getCurrentUser error:", error);
+    res.status(500).json({ message: "Get current user error" });
+  }
+};
 
-    } catch (error) {
-        console.error("getCurrentUser error:", error);
-        res.status(500).json({ message: 'Get current user error' });
-    }   
-}
+export const updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { name, email, description } = req.body;
+    let photoUrl;
+    if (req.file && req.file.path) {
+      photoUrl = await uploadOnCloudinary(req.file.path);
+    }
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { name, email, description, photo: photoUrl },
+      { new: true },
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Error updating profile. ${error.message}` });
+  }
+};
