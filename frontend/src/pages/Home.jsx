e3df3a58e2cdde4c7c5a3c15cc701bfb56c8ce12
ydevansh <yaduvanshidevansh3336@gmail.com>
2026-09-11 22:34:04 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,25 +13,24 @@ function Home() {
   const navigate=useNavigate()
   return (
     <div className="w-[100%] overflow-hidden">
-      <div className="w-[100%] 1g:h-[140vh] h- [70vh] relative">
+      <div className="w-[100%] lg:h-[140vh] h-[70vh] relative">
         <Nav />
         <img
           src={home}
-          className="object-cover md:object-fill [100%] lg:h-[100%] h- [50vh]"
+          className="object-cover md:object-fill w-[100%] lg:h-[100%] h-[50vh]"
           alt=""
-        />{" "}
-        W-
-        <span className="lg:text-[70px] absolute md:text-[40px] absolute lg:top-[10%] top-[15%] w-[100%] flex items-center justify-center text-white font-bold text-[20px]">
+        />
+        <span className="lg:text-[70px] md:text-[40px] absolute lg:top-[10%] top-[15%] w-[100%] flex items-center justify-center text-white font-bold text-[20px]">
           Grow Your Skills to Advance{" "}
         </span>
-        <span className="lg:text-[70px] text-[20px] md:text-[40px] absolute lg: top-[18%] top-[20%] w-[100%] flex items-center justify-center text-white font-bold">
+        <span className="lg:text-[70px] text-[20px] md:text-[40px] absolute lg:top-[18%] top-[20%] w-[100%] flex items-center justify-center text-white font-bold">
           Your Career path
         </span>
         <div className="absolute lg:top-[30%] top-[75%] md:top-[80%] w-[100%] flex items-center justify-center gap-3 flex-wrap">
           <button className="px-[20px] py-[10px] border-2 lg:border-white border-black lg:text-white text-black rounded-[10px] text-[18px] font-light flex gap-2 cursor-pointer" onClick={()=>navigate("/allcourses")}>
             View All Courses <SiViaplay className="w-[30px] h-[30px] lg:fill-white fill-black " />
           </button>
-          <button className="px-[20px] py-[10px] lg:bg-white bg-black lg:text-black text-white rounded-[10px] text[18px] font-light flex gap-2 cursor-pointer items-center justify-center">
+          <button className="px-[20px] py-[10px] lg:bg-white bg-black lg:text-black text-white rounded-[10px] text-[18px] font-light flex gap-2 cursor-pointer items-center justify-center">
             Search With Ai
             <img src={ai} className="w-[30px] h-[30px] rounded-full hidden lg:block" alt="" />
             <img src={ai1} className="w-[30px] h-[30px] rounded-full lg:hidden" alt="" />
