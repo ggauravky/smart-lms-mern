686c20390c658b10a2c380a71515526a0b9cc071
ydevansh <yaduvanshidevansh3336@gmail.com>
2026-09-13 00:17:55 +0530

    Populate lectures and add course back button
    Update published course fetching to populate each course's `lectures` relation so lecture data is available in API responses. Also enhance the ViewCourse page UI by adding a styled container and a back arrow button that navigates users to the home route.

--- frontend/src/pages/ViewCourse.jsx
+++ frontend/src/pages/ViewCourse.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 function ViewCourse() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
+    <div  className='min-h-screen bg-gray-50 p-6'>
+
+      <div className='max-w-6xl mx-auto bg-white shadow-md rounded-xl p-6 space-y-6 relative'>
+
+        {/* top section */}
+        <div className='w-full md:w-1/2'>
+         <FaArrowLeftLong className='text-[black] w-[22px] h-[22px] cursor-pointer' 
+         onClick={()=>navigate("/")}/>
+
+        </div>
+        </div>
     </div>
   )
 }
